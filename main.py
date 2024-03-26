53a12b6fa6ea377765134b1cd6aab5daee0d225b
AverageCog <72827009+AverageCog@users.noreply.github.com>
2024-03-25 23:24:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,12 +11,10 @@ async def on_ready():
     print(f'Logged in as {bot.user.name}')
 
 @bot.command()
-async def clear(ctx, *, user_names: str):
+async def clear(ctx, user: discord.User):
     await ctx.message.delete()
-    users = [mention for mention in ctx.message.mentions if isinstance(mention, discord.User)]  # Get all mentioned users
-    ids = list(map(lambda x: x.id, users)) + ctx.author.id  # Convert mentions to IDs and add the bot's own ID
-    
     async for message in ctx.channel.history(limit=None):
-        if any(user_id == message.author.id for user_id in ids):
+        if message.author == user:
             await message.delete()
+
 bot.run('TE4NDk0NjI5NzA2OTkxMjIxOA.GuqbuO.lKBGNKZNT07OPUoKsMVtZqDXe0nOoyOjnqM6qU')
