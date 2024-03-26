69d89d55c56d7c6f8b95e7cb2fe989ea8529e2f1
AverageCog <72827009+AverageCog@users.noreply.github.com>
2024-03-25 23:18:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,17 +4,19 @@ from discord.ext import commands
 intents = discord.Intents.all()
 intents.messages = True  # Enable the intent for message content
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='!!', intents=intents)
 
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name}')
 
 @bot.command()
-async def clear(ctx, user: discord.User):
+async def clear(ctx, *, user_names: str):
     await ctx.message.delete()
+    users = [mention for mention in ctx.message.mentions if isinstance(mention, discord.User)]  # Get all mentioned users
+    ids = list(map(lambda x: x.id, users)) + ctx.author.id  # Convert mentions to IDs and add the bot's own ID
+    
     async for message in ctx.channel.history(limit=None):
-        if message.author == user:
+        if any(user_id == message.author.id for user_id in ids):
             await message.delete()
-
 bot.run('TE4NDk0NjI5NzA2OTkxMjIxOA.GuqbuO.lKBGNKZNT07OPUoKsMVtZqDXe0nOoyOjnqM6qU')
